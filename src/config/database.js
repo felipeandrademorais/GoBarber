b25e4750f0766f3aeaca3c87c4229d6e8025fb21
unknown <flpiandrade@gmail.com>
2019-03-10 10:21:18 -0300

    Configuração BD Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: '127.0.0.1',
-  username: 'docker',
-  password: 'docker',
-  database: 'postgres',
+  host: 'ec2-54-221-243-211.compute-1.amazonaws.com',
+  username: 'vilqlwdawhtybd',
+  password: '986bab863937c98d3f144ebdc3b987bc6056a4ee53753fa612da8354952ea550',
+  database: 'd4pdhgaf3sq7ub',
   operatorAliases: false,
   define: {
     timestamps: true,
